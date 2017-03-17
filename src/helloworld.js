296ea4c4760d105695ab4923d1381d435a758dd7
hexaware34186 <vinodkumarp@hexaware.com>
2017-03-17 18:23:56 +0530

    Update helloworld.js

--- src/helloworld.js
+++ src/helloworld.js
@@ -1,8 +1,29 @@
 'use strict';
 
+const VERSION = '1.0';
+
 module.exports = function(req, res) {
 
+    if (req.body.request.type === 'LaunchRequest') {
+        res.json(
+            buildResponse(
+                { dateRequested: true },
+                '<speak>Vinod,<break time="1s"/> Hello World!</speak>',
+                {},
+                false
+            )
+        );
 
+    } else if (req.body.request.type === 'SessionEndedRequest') {
+
+       if (req.body.request.reason === 'ERROR') {
+           console.error('Alexa ended the session due to an error');
+       }
+       /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+        Per Alexa docs, we shouldn't send ANY response here... weird, I know.
+        * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+    }
 };
 
 function buildResponse(session, speech, card, end) {
@@ -19,3 +40,24 @@ function buildResponse(session, speech, card, end) {
         }
     };
 }
+
+
+// var Alexa = require("alexa-sdk");
+
+// exports.handler = function(event, context, callback) {
+//     var alexa = Alexa.handler(event, context);
+//     alexa.registerHandlers(handlers);
+//     alexa.execute();
+// };
+
+// var handlers = {
+//     'LaunchRequest': function () {
+//         this.emit('SayHello');
+//     },
+//     'HelloWorldIntent': function () {
+//         this.emit('SayHello')
+//     },
+//     'SayHello': function () {
+//         this.emit(':tell', 'Hello World!');
+//     }
+// };
