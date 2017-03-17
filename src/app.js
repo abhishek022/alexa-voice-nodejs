e41b4513d4a50c2db09da69d5b9b963497e73bac
hexaware34186 <vinodkumarp@hexaware.com>
2017-03-17 16:11:00 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,20 +1,46 @@
 'use strict';
-var Alexa = require("alexa-sdk");
 
-exports.handler = function(event, context, callback) {
-    var alexa = Alexa.handler(event, context);
-    alexa.registerHandlers(handlers);
-    alexa.execute();
-};
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var handlers = {
-    'LaunchRequest': function () {
-        this.emit('SayHello');
-    },
-    'HelloWorldIntent': function () {
-        this.emit('SayHello')
-    },
-    'SayHello': function () {
-        this.emit(':tell', 'Hello World!');
-    }
-};
+const REST_PORT = (process.env.PORT || 5000);
+const DEV_CONFIG = process.env.DEVELOPMENT_CONFIG == 'true';
+
+// console timestamps
+require('console-stamp')(console, 'yyyy.mm.dd HH:MM:ss.l');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/', (req, res) => {
+
+    console.log('POST received at root');
+    console.log(req);
+    console.log(res);
+
+});
+
+app.listen(REST_PORT, function () {
+    console.log('Rest service ready on port ' + REST_PORT);
+});
+
+// var Alexa = require("alexa-sdk");
+
+// exports.handler = function(event, context, callback) {
+//     var alexa = Alexa.handler(event, context);
+//     alexa.registerHandlers(handlers);
+//     alexa.execute();
+// };
+
+// var handlers = {
+//     'LaunchRequest': function () {
+//         this.emit('SayHello');
+//     },
+//     'HelloWorldIntent': function () {
+//         this.emit('SayHello')
+//     },
+//     'SayHello': function () {
+//         this.emit(':tell', 'Hello World!');
+//     }
+// };
