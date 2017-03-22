4e5222de730bcdc0c778981f7d39b59aa98e11c5
abhishek022 <abhishekmicro@hotmail.com>
2017-03-22 13:18:24 +0530

    Update checkindata.js

--- checkindata.js
+++ checkindata.js
@@ -1,3 +1,4 @@
+
 [
   {
     "booking_reference": "L5W4NW",
@@ -10,3 +11,59 @@
     }
   },
 ]
+
+[
+  {
+    "booking_reference": "L5W4NX",
+    "eticket_number": "176 2140736268",
+    "customer_info": {
+      "last_name": "Dash",
+      "first_name": "Abhishek",
+      "mobile_number": "+918093958952",
+      "email": "abhishekda@hexaware.com"
+    }
+  },
+]
+
+[
+  {
+    "booking_reference": "L5W4NY",
+    "eticket_number": "176 2140736269",
+    "customer_info": {
+      "last_name": "Bundel",
+      "first_name": "Lohit",
+      "mobile_number": "+918428503883",
+      "email": "pourabk@hexaware.com"
+    }
+  },
+]
+
+
+[
+  {
+    "booking_reference": "L5W4NZ",
+    "eticket_number": "176 2140736270",
+    "customer_info": {
+      "last_name": "Pourab",
+      "first_name": "Karchaudhuri",
+      "mobile_number": "+919748978812",
+      "email": "pourabk@hexaware.com"
+    }
+  },
+]
+
+  
+[
+  {
+    "booking_reference": "L5W4MA",
+    "eticket_number": "176 2140736271",
+    "customer_info": {
+      "last_name": "Surendran",
+      "first_name": "Pravin",
+      "mobile_number": "+919789031309",
+      "email": "PravinkumarS@hexaware.com"
+    }
+  },
+] 
+  
+  
