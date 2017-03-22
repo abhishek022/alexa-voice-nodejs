e93172e709e3fab97ce10d30b65329b995118b9b
abhishek022 <abhishekmicro@hotmail.com>
2017-03-22 16:00:09 +0530

    Create checkindata.js

---
+++ src/checkindata.js
@@ -0,0 +1,58 @@
+[
+  {
+    "booking_reference": "L5W4NW",
+    "eticket_number": "176 2140736267",
+    "`flight_number":"ET3678",`
+    "customer_info": {
+      "last_name": "Puliyadi",
+      "first_name": "Vinodkumar",
+      "mobile_number": "+917358599239",
+      "email": "vinodkumarp@hexaware.com"
+    }
+  },
+
+  {
+    "booking_reference": "L5W4NX",
+    "eticket_number": "176 2140736268",
+    "flight_number":"ET3456",
+    "customer_info": {
+      "last_name": "Dash",
+      "first_name": "Abhishek",
+      "mobile_number": "+918093958952",
+      "email": "abhishekda@hexaware.com"
+    }
+  },
+  {
+    "booking_reference": "L5W4NY",
+    "eticket_number": "176 2140736269",
+    "flight_number":"ET3556",
+    "customer_info": {
+      "last_name": "Bundel",
+      "first_name": "Lohit",
+      "mobile_number": "+918428503883",
+      "email": "pourabk@hexaware.com"
+    }
+  },
+  {
+    "booking_reference": "L5W4NZ",
+    "eticket_number": "176 2140736270",
+    "flight_number":"ET8976",
+    "customer_info": {
+      "last_name": "Pourab",
+      "first_name": "Karchaudhuri",
+      "mobile_number": "+919748978812",
+      "email": "pourabk@hexaware.com"
+    }
+  },
+  {
+    "booking_reference": "L5W4MA",
+    "eticket_number": "176 2140736271",
+    "flight_number":"ET8976",
+    "customer_info": {
+      "last_name": "Surendran",
+      "first_name": "Pravin",
+      "mobile_number": "+919789031309",
+      "email": "PravinkumarS@hexaware.com"
+    }
+  }
+]
