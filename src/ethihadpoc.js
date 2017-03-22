770be7a0a89c139dd50a99829a6d6c1861cc4679
hexaware34186 <hexaware34186@users.noreply.github.com>
2017-03-22 13:23:16 +0530

    initial copy

--- src/ethihadpoc.js
+++ src/ethihadpoc.js
@@ -1,1 +1,65 @@
 
+'use strict';
+
+const VERSION = '1.0';
+
+function buildSpeechletResponse(card, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "SSML",
+            ssml: output
+        },
+        card: card,
+        reprompt: {
+            outputSpeech: {
+                type: "SSML",
+                ssml: repromptText
+            },
+        },
+        shouldEndSession,
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: '1.0',
+        sessionAttributes,
+        response: speechletResponse,
+    };
+}
+
+module.exports = function(req, res) {
+
+    console.log(req.body);
+
+    let event = req.body;
+
+    try {
+        console.log(`event.session.application.applicationId=${event.session.application.applicationId}`);
+
+        if (event.session.new) {
+            onSessionStarted({ requestId: event.request.requestId }, event.session);
+        }
+
+        if (event.request.type === 'LaunchRequest') {
+            onLaunch(event.request,
+                event.session,
+                (sessionAttributes, speechletResponse) => {
+                    callback(null, buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === 'IntentRequest') {
+            onIntent(event.request,
+                event.session,
+                (sessionAttributes, speechletResponse) => {
+                    callback(null, buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === 'SessionEndedRequest') {
+            onSessionEnded(event.request, event.session);
+            callback();
+        }
+
+    } catch (err) {
+        callback(err);
+    }
+};
+
