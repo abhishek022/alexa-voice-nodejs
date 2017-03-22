19e3d3e4c27a330e02d673aa9f86d392b992553f
hexaware34186 <hexaware34186@users.noreply.github.com>
2017-03-22 12:48:34 +0530

    Create checkindata.js

---
+++ checkindata.js
@@ -0,0 +1,12 @@
+[
+  {
+    "booking_reference": "L5W4NW",
+    "eticket_number": "176 2140736267",
+    "customer_info": {
+      "last_name": "Puliyadi",
+      "first_name": "Vinodkumar",
+      "mobile_number": "+917358599239",
+      "email": "vinodkumarp@hexaware.com"
+    }
+  },
+]
