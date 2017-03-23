f1a77b54a1ec2210c09b08943cfa820caa640663
abhishek022 <abhishekmicro@hotmail.com>
2017-03-23 15:10:54 +0530

    Create flightdata.js

---
+++ flightdata.js
@@ -0,0 +1,41 @@
+[
+    {
+    "flight_number":"ET3678",
+      "departure_from":"chennai",
+      "to":"kolkata",
+      "time":"7:00 am",
+      "date":"01-4-2017",
+      "status":"ontime",
+      "terminal":"t1"
+    },
+
+    {
+    "flight_number":"ET3456",
+      "departure_from":"pune",
+      "to":"delhi",
+      "time":"8:00 am",
+      "date":"02-4-2017",
+      "status":"delayed",
+      "terminal":"t2"
+    },
+
+    {
+    "flight_number":"ET3556",
+      "departure_from":"delhi",
+      "to":"chennai",
+      "time":"9:00 pm",
+      "date":"01-4-2017",
+      "status":"canceled",
+      "terminal":"t1"
+    },
+
+    {
+    "flight_number":"ET8976",
+      "departure_from":"kolkata",
+      "to":"delhi",
+      "time":"6:00 pm",
+      "date":"02-4-2017",
+      "status":"ontime",
+      "terminal":"t2"
+    }
+]
